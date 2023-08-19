9e9487c4086572ba5db2d22d9ffa0ea10b05aff1
kTurek95 <k.turek1995@gmail.com>
2023-08-19 21:25:21 +0100

    adding docstrings

--- convert.py
+++ convert.py
@@ -1,7 +1,20 @@
+"""Module with Convert class"""
 from datetime import datetime
 
 
 class Convert:
+    """
+    A utility class for converting date strings to datetime.date objects.
+    """
     @staticmethod
     def convert_to_date(date_str):
+        """
+       Convert a date string to a datetime.date object.
+
+       Args:
+           date_str (str): A date string in the format 'YYYY-MM-DD'.
+
+       Returns:
+           datetime.date: A datetime.date object representing the converted date.
+        """
         return datetime.strptime(date_str, '%Y-%m-%d').date()

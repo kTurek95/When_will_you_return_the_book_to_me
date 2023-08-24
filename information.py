fedc4c49611831f39c2bcfc8ca37e254f6fea917
kTurek95 <k.turek1995@gmail.com>
2023-08-24 21:20:48 +0100

    Information class

---
+++ information.py
@@ -0,0 +1,31 @@
+from database import Database
+from datetime import datetime
+
+
+class Information(Database):
+    def __init__(self):
+        super().__init__()
+
+    def names_from_mail_list(self):
+        char_to_find = '@'
+        data = self.check_if_the_return_date_of_the_book_has_passed()
+        names = []
+        for mail_tuple in data:
+            mail = mail_tuple[0]
+            index_of_char = mail.find(char_to_find)
+            names.append(mail[:index_of_char])
+
+        return names
+
+    def only_delay_dates(self, today=None):
+        if today is None:
+            today = datetime.today().date()
+        dates = self.when_was_return_date()
+        delay_dates = []
+        for date in dates:
+            if date < today:
+                date_obj = datetime.strptime(date.strftime('%Y-%m-%d'), '%Y-%m-%d').date()
+                delay_dates.append(date_obj)
+
+        return delay_dates
+

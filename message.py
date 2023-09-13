c47aa61247d7e4489d8a7d87cac4c3b2ec7043be
kTurek95 <k.turek1995@gmail.com>
2023-09-13 13:07:51 +0100

    adding docstrings

--- message.py
+++ message.py
@@ -1,15 +1,41 @@
+"""
+Module: message
+
+This module provides a class, Message, that inherits from the Information class.
+ The Message class is responsible for
+formatting and sending email notifications to users with overdue book returns.
+
+Classes:
+- Message: Inherits from Information and contains methods to format and send email notifications.
+"""
 import smtplib
 from os import getenv
 from string import Template
-from dotenv import load_dotenv
 from datetime import datetime
+from dotenv import load_dotenv
 from information import Information
 
 load_dotenv()
 
 
 class Message(Information):
+    """
+    Methods:
+    - message_format(today=None):
+     Formats a message to inform users about their overdue book returns.
+    - smtp_settings(): Retrieves SMTP server settings from environment variables.
+    - send_mail(): Sends email messages to users with delayed book returns.
+    """
     def message_format(self, today=None):
+        """
+        Formats a message to inform users about their overdue book returns.
+
+        Args:
+        today (datetime.date, optional): The current date. Defaults to None.
+
+        Returns:
+        list: List of formatted messages for users.
+        """
         if today is None:
             today = datetime.today().date()
         message = Template('Hello $first_name. '
@@ -30,6 +56,13 @@ class Message(Information):
 
     @staticmethod
     def smtp_settings():
+        """
+        Retrieves SMTP server settings from environment variables.
+
+        Returns:
+        tuple: A tuple containing SMTP server settings:
+         (smtp_server, smtp_port, smtp_username, smtp_password, sender).
+        """
         smtp_server = getenv('SMTP_SERVER')
         smtp_port = int(getenv('SMTP_PORT'))
         smtp_username = getenv('USERNAME')
@@ -39,6 +72,15 @@ class Message(Information):
         return smtp_server, smtp_port, smtp_username, smtp_password, sender
 
     def send_mail(self):
+        """
+        Sends email messages to users with delayed book returns.
+
+        Args:
+        None.
+
+        Returns:
+        bool: True if the messages were sent successfully.
+        """
         mails = self.check_if_the_return_date_of_the_book_has_passed()
         messages = self.message_format()
         smtp_server, smtp_port, smtp_username, smtp_password, sender = self.smtp_settings()
@@ -48,5 +90,5 @@ class Message(Information):
             for user_mail, message in zip(mails, messages):
                 if user_mail[0].startswith(message.split()[1].lower().rstrip('.')):
                     server.sendmail(sender, user_mail, message)
-                    print(f'Udało się wysłać maila do {user_mail[0]}')
+                    print(f'The email has been sent to {user_mail[0]}')
             return True
